1e0cc809d307bd143faf122c855feec988364698
klimper <klimper@klimper-IPMH61R3>
2022-11-12 21:12:57 -0300

    Fila

--- src/colecoes/Fila.java
+++ src/colecoes/Fila.java
@@ -1,5 +1,24 @@
 package colecoes;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Fila {
+	
+	public static void main(String[] args) {
+		
+		Queue<String> fila = new LinkedList<>(); 
+		
+//		fila.add("Rodrigo");
+//		fila.offer("Carla");
+//		fila.add("Miguel");
+//		fila.offer("Pedro");
+//		fila.add("Preta");
+//		fila.offer("Magrela");
+		
+		System.out.println(fila.peek()); // retorna null.
+		System.out.println(fila.element()); // retorna uma exceção.
+		
+	}
 
 }
