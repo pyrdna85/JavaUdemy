ab724c5335c15aec7a9f4cefb67e38e9123188ba
klimper <klimper@klimper-IPMH61R3>
2022-12-17 23:04:04 -0300

    linha 10

--- src/colecoes/Fila.java
+++ src/colecoes/Fila.java
@@ -9,15 +9,30 @@ public class Fila {
 		
 		Queue<String> fila = new LinkedList<>(); 
 		
-		fila.add("Rodrigo");
-		fila.offer("Carla");
-		fila.add("Miguel");
-		fila.offer("Pedro");
-		fila.add("Preta");
-		fila.offer("Magrela");
+		fila.add("Ana"); // retorna false quando a fila está cheia.
+		fila.offer("Bia"); // retorna exceção quando a fila está cheia.
+		fila.add("Carlos");
+		fila.offer("Daniel");
+		fila.add("Rafaela");
+		fila.offer("Gui");
 		
 		System.out.println(fila.peek()); // retorna null.
+		System.out.println(fila.peek());
 		System.out.println(fila.element()); // retorna uma exceção.
+		System.out.println(fila.element());
+		
+		
+		System.out.println(fila.poll());
+		System.out.println(fila.poll());
+		System.out.println(fila.poll());
+		System.out.println(fila.poll());
+		System.out.println(fila.poll());
+		System.out.println(fila.remove("Gui"));
+		
+		
+//		fila.size();
+//		fila.clear();
+//		fila.isEmpty();
 		
 	}
 
