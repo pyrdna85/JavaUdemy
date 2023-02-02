5da10aba4bc9c06caeb45a6544fd0dd3415167e8
pyrdna85 <conta.rdna85@gmail.com>
2023-02-02 18:13:43 -0300

    teste branch

--- src/arrays/DesafioComDoisLacos.java
+++ src/arrays/DesafioComDoisLacos.java
@@ -6,7 +6,7 @@ public class DesafioComDoisLacos {
 	
 	public static void main(String[] args) {		
 		Scanner entrada = new Scanner(System.in);
-		System.out.println("Teste Branch.");
+		System.out.println("Teste Branch..");
 		
 		System.out.println("Informe quantas notas voc� quer inserir: ");
 		
